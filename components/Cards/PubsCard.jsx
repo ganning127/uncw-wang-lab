f869acdaa9c8aa9de78dcf1f90b53c64a4f53586
Ganning Xu <ganningxu@gmail.com>
2022-04-10 12:10:36 -0400

    merge

--- components/Cards/PubsCard.jsx
+++ components/Cards/PubsCard.jsx
@@ -1,20 +1,20 @@
-import { Text, HStack, VStack } from "@chakra-ui/react"
+import { Text, HStack, VStack, Img } from "@chakra-ui/react"
 
 export const PubsCard = ({ names, title, resources, publink, images }) => {
     return (
 
         <VStack>
 
-            <HStack> 
+            <HStack>
                 <Text fontWeight="normal" color='gray.800' fontSize="lg">{names}</Text>
                 <Text fontWeight="bold" color='gray.800' fontSize="lg">{title}</Text>
                 <Text fontWeight="normal" color='gray.800' fontSize="lg">{resources}</Text>
             </HStack>
 
             <HStack>
-                <Text textAlign='left' fontWeight="normal" color ='gray.800'>Button</Text>
+                <Text textAlign='left' fontWeight="normal" color='gray.800'>Button</Text>
                 <Text textAlign='center' fontWeight="bold" color='gray.800' fontSize="lg">Team Authors: </Text>
-                <img src={images} height='50px' width='50px' />
+                <Img src={images} height='50px' width='50px' />
             </HStack>
 
         </VStack>
