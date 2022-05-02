acbbd640434b9470b711576a33c0a77abf0ba231
landon-lc <casstevens75@gmail.com>
2022-05-02 19:15:12 -0400

    S V11

--- pages/publications.js
+++ pages/publications.js
@@ -56,7 +56,8 @@ export default function Publications() {
 
         <SimpleGrid columns={{ base: 1, md: 1, md: 1 }} spacing={5}>
 
-          <Heading as="h1" size="2xl" color='#263864' mb="4" fontWeight={700} alignItems = "center" textAlign = "center" spacing = {14}>Pre - The Wang Lab</Heading>
+          <Heading spacing = {20}></Heading>
+          <Heading as="h1" size="2xl" color='#263864' mb="4" fontWeight={700} alignItems = "center" textAlign = "center" spacing = {10}>Pre - The Wang Lab</Heading>
 
           <PubsCard
             names="Wang D, Wang D, Wang, D"
