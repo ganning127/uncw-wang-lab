f6b0988e0644a63c7dd20f7930bbcf2b996434c1
landon-lc <casstevens75@gmail.com>
2022-05-02 17:46:31 -0400

    Final Pub Format (Hopefully)

--- pages/publications.js
+++ pages/publications.js
@@ -24,7 +24,7 @@ export default function Publications() {
       <Banner>Publications</Banner>
 
       <Container maxW="container.xl" p={15}>
-        <SimpleGrid columns={{ base: 1, md: 1, md: 1 }}>
+        <SimpleGrid columns={{ base: 1, md: 1, md: 1 }} spacing={10}>
           <PubsCard
             names="Wang D, Wang D, Wang, D"
             title="Long Title Example for Research Paper Under the Wang Laboratory"
