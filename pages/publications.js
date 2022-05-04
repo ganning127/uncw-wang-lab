d0c20754f4adf6fc51a04ab471804da295c46c79
landon-lc <casstevens75@gmail.com>
2022-05-03 21:11:32 -0400

    Pubs 15-18 No Photos

--- pages/publications.js
+++ pages/publications.js
@@ -25,36 +25,36 @@ export default function Publications() {
       <Container maxW="container.xl" p={15}>
         <SimpleGrid columns={{ base: 1, md: 1, md: 1 }} spacing={5}>
           <PubsCard
-            names="Wang D, Wang D, Wang, D"
-            title="Long Title Example for Research Paper Under the Wang Laboratory"
-            publication= "Untitled Resource"
-            resources="15-2. 2022"
-            publink="uncw.edu"
+            names="M. J. Scannell, M. W. Hyatt, I. L. Budyak, M. A. Woldeyes, Y. Wang"
+            title="Revisit PEG-Induced Precipitation Assay for Protein Solubility Assessment of Monoclonal Antibody Formulations"
+            publication= "Pharmaceutical Research"
+            resources="PMID: 34647231 (2021)"
+            publink="https://pubmed.ncbi.nlm.nih.gov/34647231/"
+            images={["/johndoe.png", "/johndoe.png", "/johndoe.png", "/johndoe.png"]}
+          />
+          <PubsCard
+            names="Y. Wang, R. F. Latypov"
+            title="Quantitative Evaluation of Protein Solubility in Aqueous Solutions by PEG-Induced Liquid-Liquid Phase Separation"
+            publication= "Methods in Molecular Biology"
+            resources="2039, 39-49 (2019)"
+            publink="https://pubmed.ncbi.nlm.nih.gov/31342417/"
+            images={["/johndoe.png", "/johndoe.png"]}
+          />
+          <PubsCard
+            names="J.B. Rowe, R.P. Flynn, H.R. Wooten, H.A. Noufer, R.A. Cancel, J. Zhang, J.A. Subramony, S. Pechenov, Y. Wang"
+            title="Submicron Aggregation of Chemically Denatured Monoclonal Antibody"
+            publication= "Molecular Pharmaceutics"
+            resources="15, 4710-4721 (2018)"
+            publink="https://pubmed.ncbi.nlm.nih.gov/30142275/"
             images={["/johndoe.png", "/johndoe.png", "/johndoe.png"]}
           />
           <PubsCard
-            names="Wang D, Wang D, Wang, D"
-            title="Long Title Example for Research Paper Under the Wang Laboratory"
-            publication= "Untitled Resource"
-            resources="15-2. 2022"
-            publink="uncw.edu"
-            images={["/johndoe.png", "/johndoe.png", "/johndoe.png"]}
-          />
-          <PubsCard
-            names="Wang D, Wang D, Wang, D"
-            title="Long Title Example for Research Paper Under the Wang Laboratory"
-            publication= "Untitled Resource"
-            resources="15-2. 2022"
-            publink="uncw.edu"
-            images={["/johndoe.png", "/johndoe.png", "/johndoe.png"]}
-          />
-          <PubsCard
-            names="Wang D, Wang D, Wang, D"
-            title="Long Title Example for Research Paper Under the Wang Laboratory"
-            publication= "Untitled Resource"
-            resources="15-2. 2022"
-            publink="uncw.edu"
-            images={["/johndoe.png", "/johndoe.png", "/johndoe.png"]}
+            names="J.B. Rowe, R.A. Cancel, T.D. Evangelous, R.P. Flynn, S. Pechenov, J.A. Subramony, J. Zhang, Y. Wang"
+            title="Meta-stability Gap in the Phase Diagram of Monoclonal IgG Antibody"
+            publication= "Biophysical Journal"
+            resources="113, 1750-1756 (2017)"
+            publink="https://pubmed.ncbi.nlm.nih.gov/29045869/"
+            images={["/johndoe.png", "/johndoe.png", "/johndoe.png", "/johndoe.png", "/johndoe.png", "/johndoe.png"]}
           />
         </SimpleGrid>
 
