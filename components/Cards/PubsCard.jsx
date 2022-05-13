aae2b4cc59a752c891a9714a31118d49684a0c14
landon-lc <casstevens75@gmail.com>
2022-05-12 21:05:00 -0400

    klajdsf

--- components/Cards/PubsCard.jsx
+++ components/Cards/PubsCard.jsx
@@ -12,14 +12,7 @@ import {
   Spacer,
 } from "@chakra-ui/react";
 
-export const PubsCard = ({
-  names,
-  title,
-  publication,
-  resources,
-  publink,
-  images,
-}) => {
+const DesktopPubsCard = ({names, title, publication, resources, publink, images}) => {
   return (
     <Box mx="auto" rounded="lg" bg="white" shadow="md" p={15}>
       <Text>
@@ -78,4 +71,70 @@ export const PubsCard = ({
       </Flex>
     </Box>
   );
+};
+
+const MobilePubsCard = ({names, title, publication, resources, publink, images}) => {
+  return (
+    <Box mx="auto" rounded="lg" bg="white" shadow="md" p={15}>
+      <Text>
+        <Text as="span" fontWeight="normal" color="gray.800" fontSize="lg">
+          {names + ". "}
+        </Text>
+        <Text as="span" fontWeight="bold" color="gray.800" fontSize="lg">
+          {title + ". "}
+        </Text>
+        <Text as="i" fontWeight="normal" color="gray.800" fontSize="lg">
+          {publication + ". "}
+        </Text>
+        <Text as="span" fontWeight="normal" color="gray.800" fontSize="lg">
+          {resources + ". "}
+        </Text>
+      </Text>
+
+      <Flex align="center">
+        <Button
+          bg="#5BBEE5"
+          color="white"
+          _hover={{
+            bg: "#40b7e6",
+          }}
+          as="a"
+          href={publink}
+          isExternal
+        >
+          PubMed
+        </Button>
+
+        <Spacer />
+        <Box d="flex" alignItems="center">
+          <Text
+            textAlign="center"
+            fontWeight="bold"
+            color="gray.800"
+            fontSize="lg"
+          >
+            Team Authors:
+          </Text>
+          <HStack spacing={2} ml={4}>
+            {images.map((image, i) => {
+              return (
+                <Avatar
+                  src={image}
+                  height="50px"
+                  width="50px"
+                  name={names}
+                  key={i}
+                />
+              );
+            })}
+          </HStack>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export const PubsCard = {
+  Mobile: MobilePubsCard,
+  Desktop: DesktopPubsCard,
 };
