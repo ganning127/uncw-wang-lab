5d9bbafef4dd920f89ad0acdc9a9890f4dd0f3da
landon-lc <casstevens75@gmail.com>
2022-06-01 18:47:37 -0400

    heading width change

--- components/Headings/HeadingWithDesc.jsx
+++ components/Headings/HeadingWithDesc.jsx
@@ -2,7 +2,7 @@ import { Heading, Text, Box } from "@chakra-ui/react";
 export const HeadingWithDesc = (props) => {
   const { desc, children } = props;
   return (
-    <Box textAlign="center" maxW="700px" mx="auto" {...props}>
+    <Box textAlign="center" maxW="1200px" mx="auto" {...props}>
       <Heading as="h1" size="2xl" color="#263864" mb="4" fontWeight={700}>
         {children}
       </Heading>
